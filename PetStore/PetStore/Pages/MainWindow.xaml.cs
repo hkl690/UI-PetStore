afecf16cb6e6489f1c95012ad94690aa5254383c
Jina Wilde <jina.wilde@my.utsa.edu>
2023-01-22 12:21:03 -0600

    Starting to add search functionality

--- PetStore/PetStore/Pages/MainWindow.xaml.cs
+++ PetStore/PetStore/Pages/MainWindow.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Drawing;
 using System.IO;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 
 namespace PetStore
 {
@@ -22,38 +24,58 @@ namespace PetStore
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ObservableCollection<Item> items = new ObservableCollection<Item>();
+        public ObservableCollection<Item> Items
+        {
+            get { return items; }
+        }
+        
+
+
         public MainWindow()
         {
-            InitializeComponent();
+            items.Add(new Item("Test1", 0.99, "Tes2", "Test3", System.Drawing.Image.FromFile(Directory.GetCurrentDirectory() + "\\Images\\Dog\\Dog.jfif")));
+            items.Add(new Item("Test1", 0.99, "Test2", "Test3", System.Drawing.Image.FromFile(Directory.GetCurrentDirectory() + "\\Images\\Dog\\Dog.jfif")));
+            items.Add(new Item("Test4", 1.99, "Test", "Test6", System.Drawing.Image.FromFile(Directory.GetCurrentDirectory() + "\\Images\\Dog\\Dog.jfif")));
+            items.Add(new Item("Test4", 1.99, "Test5", "Test6", System.Drawing.Image.FromFile(Directory.GetCurrentDirectory() + "\\Images\\Dog\\Dog.jfif")));
+          
+                InitializeComponent();
+            SearchBox.Text = "Search";/*
              DataContext = new List<Item>
              {
              //    new Item("Test1", 0.98, "Test2", "Test3")
-             };
+             };*/
         }
 
         private void DogButton_Click(object sender, RoutedEventArgs e)
         {
             Page_Control.SelectedIndex = 1;
-            Product_Grid.Items.Add(new Item("Test1", 0.99, "Test2", "Test3", System.Drawing.Image.FromFile(Directory.GetCurrentDirectory()+"\\Images\\Dog\\Dog.jfif")));
-            Product_Grid.Columns.Add(new DataGridTextColumn { Header = "Species", Binding = new Binding("Species") });
-            Product_Grid.Columns.Add(new DataGridTextColumn { Header = "Price", Binding = new Binding("Price") });
-            Product_Grid.Columns.Add(new DataGridTextColumn { Header = "Item Name", Binding = new Binding("ItemName") });
-            Product_Grid.Columns.Add(new DataGridTextColumn { Header = "Category", Binding = new Binding("Category") });
-            /*
-                    Page_Control.SelectedIndex = 1;
+            foreach (var item in items)
+            {
+                if (!item.ItemName.Contains(SearchBox.Text)) continue;
+                System.Diagnostics.Debug.WriteLine(item.Species + item.Price + item.ItemName + item.Category + item.Picture.ToString());
+            }
+            ;
+            /* Product_Grid.Items.Add(new Item("Test1", 0.99, "Test2", "Test3", System.Drawing.Image.FromFile(Directory.GetCurrentDirectory()+"\\Images\\Dog\\Dog.jfif")));
+             Product_Grid.Columns.Add(new DataGridTextColumn { Header = "Species", Binding = new Binding("Species") });
+             Product_Grid.Columns.Add(new DataGridTextColumn { Header = "Price", Binding = new Binding("Price") });
+             Product_Grid.Columns.Add(new DataGridTextColumn { Header = "Item Name", Binding = new Binding("ItemName") });
+             Product_Grid.Columns.Add(new DataGridTextColumn { Header = "Category", Binding = new Binding("Category") });
+            */ /*
+                     Page_Control.SelectedIndex = 1;
 
-                    Product_Grid.Items.Add(new Item("Test1", 0.99, "Test2", "Test3", System.Drawing.Image.FromFile(Directory.GetCurrentDirectory()+"\\Images\\Dog\\Dog.jfif")));
-                    Product_Grid.Columns.Add(new DataGridTextColumn { Header = "Species", Binding = new Binding("Species") });
-                    Product_Grid.Columns.Add(new DataGridTextColumn { Header = "Price", Binding = new Binding("Price") });
-                    Product_Grid.Columns.Add(new DataGridTextColumn { Header = "Item Name", Binding = new Binding("ItemName") });
-                    Product_Grid.Columns.Add(new DataGridTextColumn { Header = "Category", Binding = new Binding("Category") });
-                    Product_Grid.Columns.Add(new DataGridTextColumn { Header = "Picture", Binding = new Binding("Picture") });
+                     Product_Grid.Items.Add(new Item("Test1", 0.99, "Test2", "Test3", System.Drawing.Image.FromFile(Directory.GetCurrentDirectory()+"\\Images\\Dog\\Dog.jfif")));
+                     Product_Grid.Columns.Add(new DataGridTextColumn { Header = "Species", Binding = new Binding("Species") });
+                     Product_Grid.Columns.Add(new DataGridTextColumn { Header = "Price", Binding = new Binding("Price") });
+                     Product_Grid.Columns.Add(new DataGridTextColumn { Header = "Item Name", Binding = new Binding("ItemName") });
+                     Product_Grid.Columns.Add(new DataGridTextColumn { Header = "Category", Binding = new Binding("Category") });
+                     Product_Grid.Columns.Add(new DataGridTextColumn { Header = "Picture", Binding = new Binding("Picture") });
 
-                    /*
-                    DataGridTextColumn textColumn1 = new DataGridTextColumn();
-                    textColumn1.Header = "Picture";
-                    textColumn1.Binding = new Binding("Picture");
-                    DataTemplate.Add(textColumn1);  */
+                     /*
+                     DataGridTextColumn textColumn1 = new DataGridTextColumn();
+                     textColumn1.Header = "Picture";
+                     textColumn1.Binding = new Binding("Picture");
+                     DataTemplate.Add(textColumn1);  */
 
             List<Item> Items = new List<Item>();
             //ItemsSource.Columns.Add(new Item("Test1", 0.99, "Test2", "Test3", System.Drawing.Image.FromFile(Directory.GetCurrentDirectory() + "\\Images\\Dog\\Dog.jfif"))); */
@@ -77,6 +99,24 @@ namespace PetStore
 
         }
 
+        private void RemoveSearchFromBox(object sender, RoutedEventArgs e)
+        {
+            if (SearchBox.Text.Equals("Search")) SearchBox.Text = "";
+        }
+
+        private void AddSearchToBox(object sender, RoutedEventArgs e)
+        {
+            if (SearchBox.Text.Equals("")) SearchBox.Text = "Search";
+        }
+
+        private void OnKeyDownHandler(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                SearchBox.Text = "You entered: " + SearchBox.Text;
+                // Next need to go to the search results page...
+            }
+        }
     }
 }
 /* Using this website:
